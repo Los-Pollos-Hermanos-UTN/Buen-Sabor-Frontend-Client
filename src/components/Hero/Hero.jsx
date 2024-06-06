078e9f5ea3620c346ec22cc924fbdc67ffd1a24a
Vata74 <vatatamola@gmail.com>
2024-06-06 00:23:31 -0300

    Adding Link to menu

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
                                 satisfaga tus antojos y te haga sonreír con cada mordisco.
                             </p>
                             <div>
-                                <Link to="/">
+                                <Link to="/menu">
                                     <button
                                         className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
                                         Pedir ahora
